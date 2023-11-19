9bd27a0f884bedd9c998caa0d9a6c885cbd005fa
meep334 <geraldd459@gmail.com>
2023-11-19 19:40:53 +0100

    feat(google): add google protobuf typings

--- gen_grpc_client.sh
+++ gen_grpc_client.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/env bash
-
-export PROTOBUF=/opt/protobuf
-export GOOGLEAPIS=/opt/googleapis
-GEN_DIR=lib/gen
-
-if [ ! -d "$PROTOBUF" ]; then
-  echo "Please set the PROTOBUF environment variable to your clone of protocolbuffers/protobuf."
-  exit -1
-fi
-
-if [ ! -d "$GOOGLEAPIS" ]; then
-  echo "Please set the GOOGLEAPIS environment variable to your clone of googleapis/googleapis."
-  exit -1
-fi
-
-PROTOC="protoc --dart_out=grpc:$GEN_DIR -I$PROTOBUF/src -I$GOOGLEAPIS"
-
-$PROTOC $GOOGLEAPIS/google/rpc/status.proto
-
-$PROTOC $PROTOBUF/src/google/protobuf/any.proto
-$PROTOC $PROTOBUF/src/google/protobuf/duration.proto
-$PROTOC $PROTOBUF/src/google/protobuf/empty.proto
-$PROTOC $PROTOBUF/src/google/protobuf/struct.proto
-$PROTOC $PROTOBUF/src/google/protobuf/timestamp.proto
-
-dart format $GEN_DIR/google
+./gen_google_grpc.sh
 
 PROTO_PATH=`pwd`/../rumgap/proto
 PROTOS=`find "${PROTO_PATH}" -type f -name '*.proto'`
